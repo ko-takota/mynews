3f5b73274af298c92b166a9cb3f8863e6db6ee44
kota <kou_uta.1@icloud.com>
2023-05-31 06:31:50 +0000

    課題【PHP/Laravel】06

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -3,14 +3,14 @@
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'プロフィールの新規作成')
+@section('title', '課題の作成')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィールの新規作成</h2>
+                <h2>【PHP/Laravel】06課題</h2>
             </div>
         </div>
     </div>
