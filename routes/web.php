ca91175d47f6befcd8fee7ddff6408b16d41dea9
kota <kou_uta.1@icloud.com>
2023-05-31 09:13:02 +0000

    【PHP/Laravel】08追加

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
+
 });
 
 //課題１
-Route::controller(AAAController::class)->group(function() {
-    Route::get('XXX', 'bbb');
-});
+// Route::controller(AAAController::class)->group(function() {
+//     Route::get('XXX', 'bbb');
+// });
 
 //課題２
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
     Route::get('profile/create', 'add')->middleware('auth');
     Route::get('profile/edit', 'edit')->middleware('auth');
+});
+
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+     Route::post('profile/create', 'create')->name('profile.create');
+     Route::post('profile/edit', 'update')->name('profile.edit');
 });
 
 
